7d2e4a06a825840cb0ddca86513f32ed83362fc7
chayhan <kcw2205@naver.com>
2024-02-08 15:18:10 +0900

    Add CustomNode Example

--- src/node-example/index.tsx
+++ src/node-example/index.tsx
@@ -6,6 +6,12 @@ import { DiamondNodeFactory } from './CustomNodeFactory';
 import { SimplePortFactory } from './SimplePortFactory';
 import { DiamondPortModel } from './CustomPortModel';
 import { CanvasWidget } from '@projectstorm/react-canvas-core';
+import { VariableNodeModel } from '../node/VariableNodeModel';
+import { VariableNodeFactory } from '../node/VariableNodeFactory';
+import { CustomPortModel } from '../port/CustomPortModel';
+import { CustomPortFactory } from '../port/CustomPortFactory';
+import { LayerNodeFactory } from '../node/LayerNodeFactory';
+import { ActivationType, LayerNodeModel, LayerType } from '../node/LayerNodeModel';
 
 interface CustomCanvasProp {
 	className?:string;
@@ -18,59 +24,51 @@ export default function CustomNodeExample(props:CustomCanvasProp) {
 	// register some other factories as well
 	engine
 		.getPortFactories()
-		.registerFactory(new SimplePortFactory('diamond', (config) => new DiamondPortModel(PortModelAlignment.LEFT)));
+		.registerFactory(new SimplePortFactory('diamond', () => new DiamondPortModel(PortModelAlignment.LEFT)));
 	engine.getNodeFactories().registerFactory(new DiamondNodeFactory());
+
+	engine.getPortFactories().registerFactory(new CustomPortFactory("custom_port", ()=> new CustomPortModel("default")));
+	engine.getNodeFactories().registerFactory(new VariableNodeFactory());
+	engine.getNodeFactories().registerFactory(new LayerNodeFactory());
 
 	//2) setup the diagram model
 	var model = new DiagramModel();
 
 	//3-A) create a default node
 	var node1 = new DefaultNodeModel('Node 1', 'rgb(0,192,255)');
-	var port1 = node1.addOutPort('Out');
 	node1.setPosition(100, 200);
-
 	//3-B) create our new custom node
-	var node2 = new DiamondNodeModel();
-	node2.setPosition(250, 108);
-	node2.mte = "sex";
-	var node3 = new DefaultNodeModel('Node 3', 'red');
-	var port3 = node3.addInPort('In');
-	node3.setPosition(500, 100);
 
 	//3-C) link the 2 nodes together
-	const left = node2.getPort(PortModelAlignment.LEFT);
-	const right = node2.getPort(PortModelAlignment.RIGHT);
-	let link1;
-	if(left) link1 = port1.link(left);
-	let link2;
-	if(right) link2 = port3.link(right);
 
-	var node4 = new DefaultNodeModel('Node 4', 'rgb(0,192,255)');
-	var port4 = node4.addOutPort('Out');
-	node4.setPosition(200, 10);
 
-	const top = node2.getPort(PortModelAlignment.TOP);
-	let link3;
-	if(top) link3 = port4.link(top);
-
-	var node5 = new DefaultNodeModel('Node 5', 'mediumpurple');
-	var port5 = node5.addInPort('In');
-	node5.setPosition(400, 300);
-
-	const bottom = node2.getPort(PortModelAlignment.BOTTOM);
-	let link4;
-	if(bottom) link4 = port5.link(bottom);
-
+	// 잠그기도 가능.
+	// link1?.setLocked(true);
+	// link2?.setLocked(true);
+	// link3?.setLocked(true);
+	// link4?.setLocked(true);
 	//4) add the models to the root graph
-	if(link1 && link2 && link3 && link4) 
-		model.addAll(node1, node2, node3, link1, link2, node4, link3, link4, node5);
 
+	let node_1 = new VariableNodeModel("test in", true);
+
+	let node_2 = new VariableNodeModel("test out", false);
+	// if(link1 && link2 && link3 && link4) 
+	// 	model.addAll(node1, node2, node3, node4, link3, link4, node5, node_1, node_2, new VariableNodeModel("test out2", true));
+
+	model.addAll(node_1, node_2, new VariableNodeModel("test2", false), new LayerNodeModel({name:"Layer 3", type:LayerType.LINEAR, activation:ActivationType.RELU, inputNum:2, outputNum:5}));
 	//5) load model into engine
-	engine.setModel(model);
-
+	// engine.setModel(model);
+	let testData = JSON.stringify(model.serialize());
+	console.log("모델의 링크 갯수")
+	console.log("before serialize");
 	console.log(model.serialize());
+	model.deserializeModel(JSON.parse(testData), engine);
+	console.log("after serialize");
+	console.log(model.serialize());
+	engine.setModel(model);
 	//6) render the diagram!
+	
 	return (
 		<CanvasWidget className={props.className} engine={engine} />
-	);
-};
+	)
+}
