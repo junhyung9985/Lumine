799a9db93914f6ea6c51540a60394764b5359a72
junhyung9985 <21800180@handong.edu>
2024-02-24 19:34:23 +0900

    Updated Python Logic
    Updated
    -\src\python\prototype.py (수정 사항들은 밑에 세세히 적어두었음.)
    
    Class definition이 수정되었습니다.
    - Node -> Layer로 class 이름 변경.
    - Layer class에 layer의 이름을 따로 받는 name이라는 attribute 추가.
    - Variable class의 connected_to를 int -> [int] 형태로 받도록 변경. // 일단 필요는 없어보여서 comment 처리 해두긴 함.
    - Variable class에 어느 layer의 output인지 connected_from이라는 attribute 추가. // 일단 필요는 없어보여서 comment 처리 해두긴 함.
    
    JsonParse 함수 구현이 되었습니다.
    
    WritePyTorch 함수 대거 수정되었습니다.
    - 함수에서 요구하는 Argument들 수정됨.
    - 함수 내부의 로직도 변경됨.
    	- BFS -> Topological Sort
    	- 그래프 탐색시 Variable도 하나의 노드로써 탐색함.
    	- Layer / Variable에 대한 이름 자동생성 -> 유저가 작성한 이름으로 생성. (Custom naming 가능해짐.)

--- src/python/prototype.py
+++ src/python/prototype.py
@@ -4,15 +4,17 @@
 
 import sys
 import json
+from typing import List
 
 ####################################### CLASS DEFINITION ####################################################
-class Node:
-    def __init__(self, name : str, input : int, output : int, activation : str | None = None , **kwargs):
+class Layer:
+    def __init__(self, name:str, layerType: str, input : int, output : int, activation : str | None = None , **kwargs):
         '''
             Class to store about the information of the layer + activation function that follows.
 
             Args :
-                name : the name of the layer that Node covers -> ['linear', 'convolution']
+                name : name of the layer
+                layerType : the name of the layer that Node covers -> ['linear', 'convolution']
                 input : the size of the input variable
                 output : the size of the output variable
                 activation : the name of the activation function after the layer -> ['sigmoid', 'relu']
@@ -21,13 +23,14 @@ class Node:
 
         '''
         self.name = name
+        self.layerType = layerType
         self.input = input
         self.output = output
         self.activation = activation
         self.kwargs = kwargs # channel 등등의 정보
 
 class Variable:
-    def __init__(self, name : str, connected_to : int, is_input : bool):
+    def __init__(self, name : str, connected_to : List[int] | None = None, connected_from : List[int] | None = None):
         '''
             !!! There will be significant changes on this class definition "Input" -> "Variable"
 
@@ -35,12 +38,17 @@ class Variable:
 
             Args:
                 name : name of the input variable
-                connected_to : index of which layer does this variable gives as an input
+                connected_to : list of indexes of which layer does this variable is an input.
+                connected_from : list of indexes of which layer does this variable is an output. 
         '''
         self.name = name
-        self.connected_to = connected_to
-        self.is_input = is_input
+        # self.connected_to = connected_to
+        # self.connected_from = connected_from
+        # 의외로 필요 없을 것 같아서 일단 comment 처리해둠.
 ####################################### CLASS DEFINITION ####################################################
+
+
+
 
 ####################################### PARSING LOGIC #######################################################
 
@@ -53,30 +61,51 @@ def JsonParse(json_str : str):
 
     '''
     dic = json.loads(json_str)
-    print(dic['layers'][0])
-    print(dic['layers'][1]['models'])
+    # print(dic['layers'][0])
+    # print(dic['layers'][1]['models'])
 
     node_idx = {}
     nodes = []
     cnt = 0
     inputs = []
     outputs = []
-    for node_id, infos in dic['layers'][1]['models']:
-        print(infos)
+    structure = []
+    structure2 = []
+    ind = []
+    oud = []
+    for node_id in dic['layers'][1]['models']:
+        
+        infos = dic['layers'][1]['models'][node_id]
+        node_idx[node_id] = cnt
+        structure.append([])
+        structure2.append([])
+        ind.append(0)
+        oud.append(0)
+
+        cnt += 1
+
         if(dic['layers'][1]['models'][node_id]['type'] == 'layer'):
-            infos = dic['layers'][1]['models'][node_id]
-            node_idx[node_id] = cnt
-            nodes.append(Node(infos['linear'],infos['inputNum'], infos['outputNum'], infos['activation']))
-            cnt += 1
+            nodes.append(Layer(infos['name'],infos['layerType'],infos['inputNum'], infos['outputNum'], infos['activation']))
+        elif(dic['layers'][1]['models'][node_id]['type'] == 'variable'):
+            nodes.append(Variable(infos['name']))
+        else :
+            print("ERROR : {} type is not supported".format(dic['layers'][1]['models'][node_id]['type']))
+            exit(-1)
 
+    for port in dic['layers'][0]['models']:
+        structure[node_idx[dic['layers'][0]['models'][port]['source']]].append(node_idx[dic['layers'][0]['models'][port]['target']])
+        structure2[node_idx[dic['layers'][0]['models'][port]['target']]].append(node_idx[dic['layers'][0]['models'][port]['source']])
+        ind[node_idx[dic['layers'][0]['models'][port]['target']]] += 1
+        oud[node_idx[dic['layers'][0]['models'][port]['source']]] += 1
+        print("{} : {} -> from {} to {}".format(port,dic['layers'][0]['models'][port], node_idx[dic['layers'][0]['models'][port]['source']], node_idx[dic['layers'][0]['models'][port]['target']]))
 
-    
+    for idx in range(cnt):
+        if(ind[idx] == 0) : inputs.append(idx)
+        if(oud[idx] == 0) : outputs.append(idx)
 
-    print(node_idx)
+    return nodes, node_idx, structure, structure2, ind, inputs, outputs
 
-    return dic
-
-def specify(node : Node) -> str:
+def specify(layer : Layer) -> str:
     '''
         Function for translating the layer information to PyTorch code.
 
@@ -84,8 +113,8 @@ def specify(node : Node) -> str:
             node : Node object to specify which layer does it covers.
     '''
     s = ""
-    if(node.name =='linear'): s+="nn.Linear"
-    s +='(' + str(node.input)+','+str(node.output)+')'
+    if(layer.name =='linear'): s+="nn.Linear"
+    s +='(' + str(layer.input)+','+str(layer.output)+')'
     return s
 
 def specify_act(name : str) -> str:
@@ -101,23 +130,22 @@ def specify_act(name : str) -> str:
 
 ####################################### PARSING LOGIC #######################################################
 
+
+
 ####################################### WRITING LOGIC #######################################################
 
-def WritePyTorch(structure:[[]], nodes : [Node],  path : str, inputs : [Variable], outputs : [Variable]) -> str:
+def WritePyTorch(nodes : List[Layer | Variable],  structure : List[List[int]], structure2 : List[List[int]], inputs : List[int], outputs : List[int], indegree : List [int]) -> str:
     '''
-        Function for writing PyTorch model class code with given nodes, input variables, and connectivities between them.
+        Function for writing PyTorch model class code with given nodes and edges.
 
         Args:
-            structure : Adjacency Matrix for connectivities between nodes. structure[i][j] = 1 if there is a path i -> j, else 0.
-            nodes : List of Node objects
-            path : path of directory that user wants to save.
-            inputs : List of Input objects
-
+            nodes : List of Layer or Variable objects.
+            structure : Adjacency List for connectivities between nodes. 
+            structure2 : Adjacency List for connectivities between nodes. Reverse of structure.
+            inputs : List of indexes for input variables.
+            outputs : List of indexes for output variables.
+            indegree : List of indegrees for each node. Used for Topological Sorting Algorithm.
     '''
-
-    # 각 노드별로 순서에 따라서 네이밍.
-    nodes_name = ['m'+str(i) for i in range(len(nodes))]
-    #print(nodes_name)
 
     # 기본적으로 출력해야 할 것들
     s = ""
@@ -126,62 +154,67 @@ def WritePyTorch(structure:[[]], nodes : [Node],  path : str, inputs : [Variable
     s += "\tdef __init__(self):\n"
     s += "\t\tsuper().__init__()\n"
 
-    # 각 노드별로 Constructor에 올려둠.
-    for name,node in zip(nodes_name, nodes):
-        s += "\t\tself."+name+" = "+specify(node)+"\n"
-        if(node.activation != None):
-            s += "\t\tself."+name+"_act = "+specify_act(node.activation)+"\n"
-
+    # 각 Layer별로 Constructor에 올려둠.
+    for node in  nodes:
+        if type(node) == Layer : 
+            s += "\t\tself."+node.name+" = "+specify(node)+"\n"
+            if(node.activation != None):
+                s += "\t\tself."+node.name+"_act = "+specify_act(node.activation)+"\n"
+        
     # Forward 메소드 작성 시작, 여기에 Input variables들도 넣어둠.
     s += "\n\tdef forward(self"
-    for x in inputs:
+    for idx in inputs:
         s += ", "
-        s += x.name
+        s += nodes[idx].name
     s += "):\n"
 
-    start_nodes = [] # Queue for storing nodes.
-
-    for x in inputs:
-        idx = x.connected_to
-        s += "\t\to{} = self.{}({})\n".format(idx, nodes_name[idx], x.name)
-        if(nodes[idx].activation != None):
-            s += "\t\to{} = self.{}_act(o{})\n".format(idx, nodes_name[idx], idx)
-        start_nodes.append(idx)
+    queue = inputs # Queue for storing nodes.
 
     ret = 0
-    while(len(start_nodes) != 0):
-        idx = start_nodes[0]
+    while(len(queue) != 0):
+        idx = queue[0]
         ret = idx
-        del(start_nodes[0])
-        for next, connected in enumerate(structure[idx]):
-            if(connected == 1):
-                s += "\t\to{} = self.{}(o{})\n".format(next, nodes_name[next], idx)
-                if(nodes[next].activation != None):
-                    s += "\t\to{} = self.{}_act(o{})\n".format(next, nodes_name[next], next)
-                start_nodes.append(next)
+        del(queue[0])
+        if(type(nodes[idx]) == Layer):
+            for bef_node in structure2[idx]:
+                if type(nodes[bef_node]) == Layer:
+                    s+= "\t\tout_{} = self.{}(out_{})\n".format(nodes[idx].name, nodes[idx].name, nodes[bef_node].name)
+                elif type(nodes[bef_node]) == Variable:
+                    s+= "\t\tout_{} = self.{}({})\n".format(nodes[idx].name, nodes[idx].name, nodes[bef_node].name)
+                else:
+                    print("ERROR : UNAVAILABLE INPUT")
+                    exit(-1)
+        elif(type(nodes[idx]) == Variable):
+            for bef_node in structure2[idx]:
+                if type(nodes[bef_node]) == Layer:
+                    s+= "\t\t{} = out_{}\n".format(nodes[idx].name, nodes[bef_node].name)
+                elif type(nodes[bef_node]) == Variable:
+                    s+= "\t\t{} = {}\n".format(nodes[idx].name, nodes[bef_node].name)
+                else:
+                    print("ERROR : UNAVAILABLE INPUT")
+                    exit(-1)
+        else :
+            print("ERROR : UNAVAILABLE INPUT")
+        
+        for next_node in structure[idx]:
+            ind[next_node] -= 1
+            if(ind[next_node] == 0):
+                queue.append(next_node)
 
-    s += "\t\treturn o{}".format(ret)
-
+    s += "\t\treturn "
+    for i, idx in enumerate(outputs) :
+        if(type(nodes[idx]) == Variable):
+            s += nodes[idx].name
+        else :
+            s += "out_"+nodes[idx].name
+        if(i < len(outputs)-1):
+            s += ", "
+    
     return s # 일단은 아무 필요 없어는 보여서 이렇게 주기는 했는데, 서버측에서 성공적으로 실행되었는지를 알아보려면 Response 값 등을 리턴하는 것도 좋아는 보임.
 
-    # BFS since we do not think about residual connections.
-    # Since we do not consider residual connection right now, every nodes indegree will be 1.
-    # So just doing BFS would be fine. We will change the algorithm to Topological Sorting if needed.
-
+    # Topological Sort to produce Forward Method.
 
 ####################################### WRITING LOGIC #######################################################
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -190,13 +223,16 @@ if __name__ == '__main__':
     print(len(sys.argv))
 
     if(len(sys.argv) > 1):
-        dic = JsonParse(sys.argv[1])
-        print(type(dic))
+        nodes, node_idx, structure, structure2, ind, inputs, outputs = JsonParse(sys.argv[1])
+        result = WritePyTorch(nodes, structure, structure2, inputs, outputs, ind)
+        print(result)
 
-    mock_structure = [[0,1,0],[0,0,1],[0,0,0]]
-    mock_nodes = [Node('linear',5,4,'relu'), Node('linear',4,3,'softmax'), Node('linear',3,1,'sigmoid')]
-    path = "./"
-    input = [Variable('x',0,True)]
-    output = [Variable('y',2,True)]
-    print(WritePyTorch(mock_structure, mock_nodes, path, input,output))
+    else :
+        print("ERROR : Cannot find JSON string.")
+    # mock_structure = [[0,1,0],[0,0,1],[0,0,0]]
+    # mock_nodes = [Layer('m1','linear',5,4,'relu'), Layer('m2', 'linear',4,3,'softmax'), Layer('m3','linear',3,1,'sigmoid')]
+    # path = "./"
+    # input = [Variable('x',[0],[])]
+    # output = [Variable('y',[2],[])]
+    # print(WritePyTorch(mock_structure, mock_nodes, path, input,output))
     
