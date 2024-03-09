1417c9387e7de597254368aa7763ba29d4eadf1e
chayhan <kcw2205@naver.com>
2024-03-09 19:51:06 +0900

    fix pyodide export config

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,6 @@ export default defineConfig({
   server:{
     port:3000 // 포트를 Nextjs 데브서버와 통일시켰음.(헷갈려서)
   },
-  build:{
-    rollupOptions:{
-      external: ['pyodide'],
-      output: {
-        globals: {
-          pyodide: 'pyodide',
-        },
-      },
-    }
-  },
   base:"/Lumine/",
   assetsInclude:[
     "**/*.py"
